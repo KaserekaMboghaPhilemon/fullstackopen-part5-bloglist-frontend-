f34adc20e78b3a9c125cd3076405ce7c02dce6fc
Philemon Kasereka <kmboghaphilemon@gmail.com>
2026-07-09 16:24:02 +0300

    feat: complete exercise 5.7 adding inline styles and local view/hide toggle for blog details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
+
 const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
-    border: 'solid',
+    border: "solid",
     borderWidth: 1,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
 
   return (
     <div style={blogStyle}>
       <div>
         {blog.title} {blog.author}
+        <button type="button" onClick={toggleVisibility}>
+          {visible ? "hide" : "view"}
+        </button>
       </div>
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button type="button">like</button>
+          </div>
+          <div>{blog.user?.name}</div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
